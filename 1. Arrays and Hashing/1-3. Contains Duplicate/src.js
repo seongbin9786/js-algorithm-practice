30ed52647229dba5a45f405659770fa7bfa239b7
Seongbin Kim <seongbin9786@gmail.com>
2023-02-07 15:48:14 +0900

    solve(Contains Duplicate): Implemented solution

---
+++ 1. Arrays and Hashing/1-3. Contains Duplicate/src.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = function (nums) {
+  //ASC  
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
