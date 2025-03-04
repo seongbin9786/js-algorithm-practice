04ebda7d6e1c7fcdc08d79ae017621f8b6c1fd35
Seongbin Kim <seongbin9786@gmail.com>
2025-03-05 07:59:32 +0900

    solve(Valid Anagram): re-check 1 - optimal

---
+++ 3. LeetCode Grind75/Valid Anagram/250305-optimal.js
@@ -0,0 +1,33 @@
+/*
+[문제]
+- 문자열 s, t를 입력 받아 t가 s의 아나그램 여부를 반환
+- 아나그램 = 모든 알파벳을 주어진 개수를 모두 써서 재구성할 수 있는지
+- s, t의 길이 5만
+- 알파벳 소문자로만 구성
+
+[해결 방법]
+- Map으로 카운팅하는 것밖에 생각이 나지 않는다.
+- 해결 가능한가?
+- 알파벳 소문자일 때는 배열 트릭을 쓰면 조금 더 낫다 ( 노 해싱 , 콜리전 )
+
+*/
+const A = "a".charCodeAt(0);
+
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    const count = Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - A]++;
+        count[t.charCodeAt(i) - A]--;
+    }
+    for (let i = 0; i < 26; i++) {
+        if (count[i] !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
