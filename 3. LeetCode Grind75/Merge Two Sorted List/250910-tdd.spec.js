7706291fc188e76efcf8a1853fe00a8f1ba61e48
Seongbin Kim <seongbin9786@gmail.com>
2025-09-10 17:20:58 +0900

    try(Merge Two Sorted Lists): 섞인 Case들

--- 3. LeetCode Grind75/Merge Two Sorted List/250910-tdd.spec.js
+++ 3. LeetCode Grind75/Merge Two Sorted List/250910-tdd.spec.js
@@ -24,6 +24,18 @@ function stringifyList(node) {
     return result.join(",");
 }
 
+function showLinkes(head) {
+    let str = `[${head?.val}]`;
+    head = head?.next;
+
+    while (head) {
+        str += `->[${head.val}]`;
+        head = head.next;
+    }
+
+    return str;
+}
+
 describe.only("Merge Two Sorted Lists", () => {
     it("[] + [] = []", () => {
         const expected = stringifyList(null);
@@ -64,16 +76,122 @@ describe.only("Merge Two Sorted Lists", () => {
 
         assert.equal(stringifyList(result), expected);
     });
+
+    it("[1,3] + [2] = [1,2,3]", () => {
+        const expected = stringifyList(parseList("1,2,3"));
+        const list1 = parseList("1,3");
+        const list2 = parseList("2");
+        const result = mergeTwoLists(list1, list2);
+        assert.equal(stringifyList(result), expected);
+    });
+
+    it("[2] + [1,3] = [1,2,3]", () => {
+        const expected = stringifyList(parseList("1,2,3"));
+        const list1 = parseList("2");
+        const list2 = parseList("1,3");
+        const result = mergeTwoLists(list1, list2);
+        assert.equal(stringifyList(result), expected);
+    });
+
+    it("[1,2] + [3] = [1,2,3]", () => {
+        // 구현 방법: 2 < 3 이므로, 3을 1,2 사이에 넣지 않고, 기존 리스트를 유지
+        // 추가 조건: list1Next && list1Next.val < list2.val
+        const expected = stringifyList(parseList("1,2,3"));
+        const list1 = parseList("1,2");
+        const list2 = parseList("3");
+        const result = mergeTwoLists(list1, list2);
+        assert.equal(stringifyList(result), expected);
+    });
+
+    it("[3] + [1,2] = [1,2,3]", () => {
+        /*
+            구현 방법:
+            [2] vs [3,4] 일 때, [2] -> [3] -> null 이 되어버리므로, 조건부로 할당해야 함.
+            if (list1Next) {
+                list2.next = list1Next; // <--- 조건부로 실행해야 함.
+            }
+        */
+        const expected = stringifyList(parseList("1,2,3"));
+        const list1 = parseList("3");
+        const list2 = parseList("1,2");
+        const result = mergeTwoLists(list1, list2);
+        assert.equal(stringifyList(result), expected);
+    });
+
+    it("[1,2] + [3,4] = [1,2,3,4]", () => {
+        const expected = stringifyList(parseList("1,2,3,4"));
+        const list1 = parseList("1,2");
+        const list2 = parseList("3,4");
+        const result = mergeTwoLists(list1, list2);
+        assert.equal(stringifyList(result), expected);
+    });
+
+    it("[1,3] + [2,4] = [1,2,3,4]", () => {
+        const expected = stringifyList(parseList("1,2,3,4"));
+        const list1 = parseList("1,3");
+        const list2 = parseList("2,4");
+        const result = mergeTwoLists(list1, list2);
+        assert.equal(stringifyList(result), expected);
+    });
+
+    it("[0,0] + [0,0] = [0,0,0,0]", () => {
+        const expected = stringifyList(parseList("0,0,0,0"));
+        const list1 = parseList("0,0");
+        const list2 = parseList("0,0");
+        const result = mergeTwoLists(list1, list2);
+        assert.equal(stringifyList(result), expected);
+    });
+
+    it.skip("[1,3] + [2,4] = [1,2,3,4]", () => {
+        const expected = stringifyList(parseList("1,2,3,4"));
+        const list1 = parseList("1,3");
+        const list2 = parseList("2,4");
+        const result = mergeTwoLists(list1, list2);
+        assert.equal(stringifyList(result), expected);
+    });
 });
 
 var mergeTwoLists = function (list1, list2) {
     if (!list1 || !list2) return list1 ?? list2;
 
-    if (list1.val >= list2.val) {
-        list2.next = list1;
-        return list2;
-    } else {
-        list1.next = list2;
-        return list1;
+    const head = list1.val >= list2.val ? list2 : list1;
+
+    // [0,0] + [0,0] = [0,0,0,0]
+    // [0,0,0,0]이 나와야 하는데 뒤에 [0]이 안 나옴.
+    while (list1 && list2) {
+        const list1Next = list1.next;
+        const list2Next = list2.next;
+        console.log(
+            `list1: [${showLinkes(list1)}], list2: [${showLinkes(list2)}]`
+        );
+
+        if (list1.val <= list2.val) {
+            if (list1Next && list1Next.val < list2.val) {
+                list1 = list1.next;
+                continue;
+            }
+            list1.next = list2;
+
+            // [2] vs [3,4] 일 때, [2] -> [3] -> null 이 되어버리므로, 조건부로 할당해야 함.
+            if (list1Next) {
+                list2.next = list1Next;
+            }
+            list1 = list1Next;
+            list2 = list2Next;
+        } else {
+            if (list2Next && list2Next.val < list1.val) {
+                list2 = list2.next;
+                continue;
+            }
+
+            list2.next = list1;
+            if (list2Next) {
+                list1.next = list2Next;
+            }
+            list1 = list1Next;
+            list2 = list2Next;
+        }
     }
+
+    return head;
 };
