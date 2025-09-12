d0df639c60a9a8a110ebc94796dee62df8513943
Seongbin Kim <seongbin9786@gmail.com>
2025-09-12 23:11:38 +0900

    try(Valid Anagram): 기본 case 구현

--- 3. LeetCode Grind75/Valid Anagram/250912-tdd.spec.js
+++ 3. LeetCode Grind75/Valid Anagram/250912-tdd.spec.js
@@ -6,13 +6,40 @@ import { describe, it, assert } from "vitest";
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    return true;
+    const aCode = "a".charCodeAt(0);
+    const counter = Array.from({ length: 30 }, () => 0); // 알파벳 몇개인지 헷갈려서 안전하게 30
+    for (let i = 0; i < s.length; i++) {
+        counter[s.charCodeAt(i) - aCode]++;
+        counter[t.charCodeAt(i) - aCode]--;
+    }
+    return !counter.find((v) => v !== 0);
 };
 
 describe.only("Valid Anagram", () => {
     it("[] = []", () => {
         const input = "";
         const expected = "";
+        const result = isAnagram(input, expected);
+        assert.equal(result, true);
+    });
+
+    it("[a] = [a]", () => {
+        const input = "a";
+        const expected = "a";
+        const result = isAnagram(input, expected);
+        assert.equal(result, true);
+    });
+
+    it("[a] = [b]", () => {
+        const input = "a";
+        const expected = "b";
+        const result = isAnagram(input, expected);
+        assert.equal(result, false);
+    });
+
+    it("[ab] = [ba]", () => {
+        const input = "ab";
+        const expected = "ba";
         const result = isAnagram(input, expected);
         assert.equal(result, true);
     });
