c4c95a8957c7a1e3187af76102f66f7a52517f1e
Seongbin Kim <seongbin9786@gmail.com>
2025-09-12 22:51:51 +0900

    try(Invert Binary Tree): 기본 Case들 구현/해결 (optimal), 너무 쉬운 문제였음

--- 3. LeetCode Grind75/Invert Binary Tree/250912-tdd.spec.js
+++ 3. LeetCode Grind75/Invert Binary Tree/250912-tdd.spec.js
@@ -3,7 +3,7 @@ import { assert, describe, it } from "vitest";
 describe.only("Invert Binary Tree", () => {
     it("null -> null", () => {
         const expected = null;
-        const result = invertBinaryTree(null);
+        const result = invertTree(null);
 
         assert.equal(result, expected);
     });
@@ -11,7 +11,21 @@ describe.only("Invert Binary Tree", () => {
     it("[1] -> [1]", () => {
         const input = createTree("1");
         const expected = createTree("1");
-        const result = invertBinaryTree(input);
+        const result = invertTree(input);
+        assert.equal(stringifyTree(result), stringifyTree(expected));
+    });
+
+    it("[1,2,3] -> [1,3,2]", () => {
+        const input = createTree("1,2,3");
+        const expected = createTree("1,3,2");
+        const result = invertTree(input);
+        assert.equal(stringifyTree(result), stringifyTree(expected));
+    });
+
+    it("[1,2,3,4,5,6,7] -> [1,3,2,7,6,5,4]", () => {
+        const input = createTree("1,2,3,4,5,6,7");
+        const expected = createTree("1,3,2,7,6,5,4");
+        const result = invertTree(input);
         assert.equal(stringifyTree(result), stringifyTree(expected));
     });
 });
@@ -54,8 +68,18 @@ function stringifyTree(tree) {
     return result.join(",");
 }
 
-function compareTree(a, b) {}
+function invertTree(tree) {
+    if (tree) {
+        const left = tree.left;
+        tree.left = tree.right;
+        tree.right = left;
 
-function invertBinaryTree(tree) {
-    return null;
+        if (tree.left) {
+            invertTree(tree.left);
+        }
+        if (tree.right) {
+            invertTree(tree.right);
+        }
+    }
+    return tree;
 }
