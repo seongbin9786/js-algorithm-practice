f8fcb4984a2dd8beb1de54a11a6386de8539f542
Seongbin Kim <seongbin9786@gmail.com>
2025-09-13 10:23:28 +0900

    try(Binary Search): 리팩토링 및 출력 형식 준수 (boolean -> idx)

--- 3. LeetCode Grind75/Binary Search/250913-tdd.spec.js
+++ 3. LeetCode Grind75/Binary Search/250913-tdd.spec.js
@@ -2,17 +2,17 @@ import { describe, it, assert } from "vitest";
 
 describe.only("Binary Search (time complexity limit: O(log n)", () => {
     it.each([
-        [1, [1], true],
-        [2, [2], true],
-        [1, [2], false],
-        [2, [1, 2], true],
-        [2, [1, 2, 3], true],
-        [3, [1, 2, 3], true], // 오, 안된다. 신기함. 구현을 반대로 한 거였음..
+        [1, [1], 0],
+        [2, [2], 0],
+        [1, [2], -1],
+        [2, [1, 2], 1],
+        [2, [1, 2, 3], 1],
+        [3, [1, 2, 3], 2], // 오, 안된다. 신기함. 구현을 반대로 한 거였음..
         // 다양한 예외 케이스들
-        [0, [1], false],
-        [2, [1], false],
-        [0, [1, 2, 3], false],
-        [4, [1, 2, 3], false],
+        [0, [1], -1],
+        [2, [1], -1],
+        [0, [1, 2, 3], -1],
+        [4, [1, 2, 3], -1],
     ])("find [%i] from %j => %s", (target, source, expected) => {
         const result = search(source, target);
         assert.equal(result, expected);
@@ -25,20 +25,18 @@ describe.only("Binary Search (time complexity limit: O(log n)", () => {
  * @return {number}
  */
 var search = function (nums, target) {
-    // 재귀도 되고, while도 되고.
-    // 일단 재귀로 풀어볼까.
+    // 재귀도 되고, while도 되는데 일단 재귀로 풀이
     const binarySearch = (start, end) => {
         if (start > end) {
-            return false;
+            return -1;
         }
         const mid = Math.floor((start + end) / 2);
         if (nums[mid] === target) {
-            return true;
+            return mid;
         }
-        if (nums[mid] > target) {
-            return binarySearch(start, mid - 1);
-        }
-        return binarySearch(mid + 1, end);
+        return nums[mid] > target
+            ? binarySearch(start, mid - 1)
+            : binarySearch(mid + 1, end);
     };
 
     return binarySearch(0, nums.length);
