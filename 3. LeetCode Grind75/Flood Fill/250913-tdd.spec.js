0af9cf4a563a07ae38f2c6c58187da57c27e6c8d
Seongbin Kim <seongbin9786@gmail.com>
2025-09-13 10:46:08 +0900

    try(Flood Fill): parameterized test 에서 일반 테스트로 복귀

--- 3. LeetCode Grind75/Flood Fill/250913-tdd.spec.js
+++ 3. LeetCode Grind75/Flood Fill/250913-tdd.spec.js
@@ -1,38 +1,38 @@
 import { describe, it, assert } from "vitest";
 
+// {
+//     image: [
+//         [1, 1, 1],
+//         [1, 1, 0],
+//         [1, 0, 1],
+//     ],
+//     sr: 1,
+//     sc: 2,
+//     color: 1,
+//     expected: [
+//         [2, 2, 2],
+//         [2, 2, 0],
+//         [2, 0, 1],
+//     ],
+// },
+
 describe.only("Flood Fill", () => {
-    it.each([
-        [
-            {
-                image: [[0]],
-                sr: 0,
-                sc: 0,
-                color: 0,
-                expected: [[0]],
-            },
-            // {
-            //     image: [
-            //         [1, 1, 1],
-            //         [1, 1, 0],
-            //         [1, 0, 1],
-            //     ],
-            //     sr: 1,
-            //     sc: 2,
-            //     color: 1,
-            //     expected: [
-            //         [2, 2, 2],
-            //         [2, 2, 0],
-            //         [2, 0, 1],
-            //     ],
-            // },
-        ],
-    ])(
-        "fill [%j] from (%i,%j) to [%i] => %j",
-        ({ image, color, sr, sc, expected }) => {
-            const result = floodFill(image, sr, sc, color);
-            assert.equal(JSON.stringify(result), JSON.stringify(expected));
-        }
-    );
+    it("fill [0] from (0,0) to 0 => [0]", () => {
+        const image = [0];
+        const color = 0;
+        const [sr, sc] = [0, 0];
+        const expected = [0];
+        const result = floodFill(image, sr, sc, color);
+        assert.equal(JSON.stringify(result), JSON.stringify(expected));
+    });
+    it("fill [0] from (0,0) to 1 => [1]", () => {
+        const image = [0];
+        const color = 1;
+        const [sr, sc] = [0, 0];
+        const expected = [1];
+        const result = floodFill(image, sr, sc, color);
+        assert.equal(JSON.stringify(result), JSON.stringify(expected));
+    });
 });
 
 /**
@@ -43,5 +43,6 @@ describe.only("Flood Fill", () => {
  * @return {number[][]}
  */
 var floodFill = function (image, sr, sc, color) {
+    image[0] = color;
     return image;
 };
