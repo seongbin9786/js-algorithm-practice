f839f24b52edd06e44242d1161d00818052c78f4
Seongbin Kim <seongbin9786@gmail.com>
2025-09-13 11:11:38 +0900

    try(Flood Fill): 2x2 케이스 구현 완료

--- 3. LeetCode Grind75/Flood Fill/250913-tdd.spec.js
+++ 3. LeetCode Grind75/Flood Fill/250913-tdd.spec.js
@@ -52,11 +52,25 @@ describe.only("Flood Fill", () => {
             [1, 0],
             [0, 0],
         ];
-        const color = 1;
+        const color = 2;
         const [sr, sc] = [0, 0];
         const expected = [
             [2, 0],
             [0, 0],
+        ];
+        const result = floodFill(image, sr, sc, color);
+        assert.equal(JSON.stringify(result), JSON.stringify(expected));
+    });
+    it("fill [[0,0],[0,1]] from (0,0) to 2 => [[2,2],[2,1]]", () => {
+        const image = [
+            [0, 0],
+            [0, 1],
+        ];
+        const color = 2;
+        const [sr, sc] = [0, 0];
+        const expected = [
+            [2, 2],
+            [2, 1],
         ];
         const result = floodFill(image, sr, sc, color);
         assert.equal(JSON.stringify(result), JSON.stringify(expected));
@@ -73,9 +87,33 @@ describe.only("Flood Fill", () => {
 var floodFill = function (image, sr, sc, color) {
     // 실제로 구현해야 함. BFS로 해야 함. 1칸씩 이동해야 하기 때문임.
     // 성능 상 image 방어적 복사 없이 쓰기 수행
-    for (let i = 0; i < image.length; i++) {
-        for (let j = 0; j < image[0].length; j++) {
-            image[i][j] = color;
+
+    const MAX_R = image.length;
+    const MAX_C = image[0].length;
+    const dr = [-1, 1, 0, 0];
+    const dc = [0, 0, -1, 1];
+
+    const targetColor = image[sr][sc];
+    const queue = [[sr, sc]];
+
+    while (queue.length > 0) {
+        const [r, c] = queue.shift();
+        const currColor = image[r][c];
+        console.log("currColor:", currColor);
+        if (currColor === targetColor) {
+            console.log("visiting:", r, c, image[r][c]);
+            image[r][c] = color;
+
+            for (let i = 0; i < 4; i++) {
+                const nr = r + dr[i];
+                const nc = c + dc[i];
+                if (nr < 0 || nr >= MAX_R || nc < 0 || nc >= MAX_C) {
+                    continue;
+                }
+                if (image[nr][nc] === targetColor) {
+                    queue.push([nr, nc]);
+                }
+            }
         }
     }
 
