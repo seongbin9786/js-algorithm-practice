34a2cdc46c456dfebcb430bbb3aa2fbe5c720637
Seongbin Kim <seongbin9786@gmail.com>
2025-09-13 11:00:18 +0900

    try(Flood Fill): TC 이해 오류 해소 (1차원 배열인 case 없음)

--- 3. LeetCode Grind75/Flood Fill/250913-tdd.spec.js
+++ 3. LeetCode Grind75/Flood Fill/250913-tdd.spec.js
@@ -17,19 +17,19 @@ import { describe, it, assert } from "vitest";
 // },
 
 describe.only("Flood Fill", () => {
-    it("fill [0] from (0,0) to 0 => [0]", () => {
-        const image = [0];
+    it("fill [[0]] from (0,0) to 0 => [0]", () => {
+        const image = [[0]];
         const color = 0;
         const [sr, sc] = [0, 0];
-        const expected = [0];
+        const expected = [[0]];
         const result = floodFill(image, sr, sc, color);
         assert.equal(JSON.stringify(result), JSON.stringify(expected));
     });
-    it("fill [0] from (0,0) to 1 => [1]", () => {
-        const image = [0];
+    it("fill [[0]] from (0,0) to 1 => [1]", () => {
+        const image = [[0]];
         const color = 1;
         const [sr, sc] = [0, 0];
-        const expected = [1];
+        const expected = [[1]];
         const result = floodFill(image, sr, sc, color);
         assert.equal(JSON.stringify(result), JSON.stringify(expected));
     });
@@ -47,6 +47,20 @@ describe.only("Flood Fill", () => {
         const result = floodFill(image, sr, sc, color);
         assert.equal(JSON.stringify(result), JSON.stringify(expected));
     });
+    it("fill [[1,0],[0,0]] from (0,0) to 2 => [[2,0],[0,0]]", () => {
+        const image = [
+            [1, 0],
+            [0, 0],
+        ];
+        const color = 1;
+        const [sr, sc] = [0, 0];
+        const expected = [
+            [2, 0],
+            [0, 0],
+        ];
+        const result = floodFill(image, sr, sc, color);
+        assert.equal(JSON.stringify(result), JSON.stringify(expected));
+    });
 });
 
 /**
@@ -57,14 +71,12 @@ describe.only("Flood Fill", () => {
  * @return {number[][]}
  */
 var floodFill = function (image, sr, sc, color) {
-    if (image[0] instanceof Array) {
-        for (let i = 0; i < image.length; i++) {
-            for (let j = 0; j < image[0].length; j++) {
-                image[i][j] = color;
-            }
+    // 실제로 구현해야 함. BFS로 해야 함. 1칸씩 이동해야 하기 때문임.
+    // 성능 상 image 방어적 복사 없이 쓰기 수행
+    for (let i = 0; i < image.length; i++) {
+        for (let j = 0; j < image[0].length; j++) {
+            image[i][j] = color;
         }
-    } else {
-        image[0] = color;
     }
 
     return image;
