e0644a994da48fa4e5d3544ba1c35b8bfba0bb24
Seongbin Kim <seongbin9786@gmail.com>
2025-09-13 10:21:08 +0900

    try(Binary Search): 예외 TC 통과 확인

--- 3. LeetCode Grind75/Binary Search/250913-tdd.spec.js
+++ 3. LeetCode Grind75/Binary Search/250913-tdd.spec.js
@@ -2,12 +2,17 @@ import { describe, it, assert } from "vitest";
 
 describe.only("Binary Search (time complexity limit: O(log n)", () => {
     it.each([
-        //[1, [1], true],
-        //[2, [2], true],
-        //[1, [2], false],
-        //[2, [1, 2], true],
-        //[2, [1, 2, 3], true],
-        [3, [1, 2, 3], true], // 오, 안된다. 신기함.
+        [1, [1], true],
+        [2, [2], true],
+        [1, [2], false],
+        [2, [1, 2], true],
+        [2, [1, 2, 3], true],
+        [3, [1, 2, 3], true], // 오, 안된다. 신기함. 구현을 반대로 한 거였음..
+        // 다양한 예외 케이스들
+        [0, [1], false],
+        [2, [1], false],
+        [0, [1, 2, 3], false],
+        [4, [1, 2, 3], false],
     ])("find [%i] from %j => %s", (target, source, expected) => {
         const result = search(source, target);
         assert.equal(result, expected);
@@ -24,15 +29,13 @@ var search = function (nums, target) {
     // 일단 재귀로 풀어볼까.
     const binarySearch = (start, end) => {
         if (start > end) {
-            // 일단 최소 구현
             return false;
         }
         const mid = Math.floor((start + end) / 2);
-        console.log("idx:", start, mid, end);
         if (nums[mid] === target) {
             return true;
         }
-        if (nums[mid] < target) {
+        if (nums[mid] > target) {
             return binarySearch(start, mid - 1);
         }
         return binarySearch(mid + 1, end);
