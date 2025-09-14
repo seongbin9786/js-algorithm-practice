7f4b300749f57d4c7d121bb6536a79ff1e9f5332
Seongbin Kim <seongbin9786@gmail.com>
2025-09-14 15:07:20 +0900

    try(Maximum Subarray): max 초기화 case 구현

--- 3. LeetCode Grind75/Maximum Subarray/250914-tdd.spec.js
+++ 3. LeetCode Grind75/Maximum Subarray/250914-tdd.spec.js
@@ -37,6 +37,8 @@ describe.only("Maximum Subarray (time complexity limit: O(n))", () => {
         [[0, 1], 1],
         [[0, -1], 0],
         [[-1, 0], 0],
+        [[-1, 2, -1], 2],
+        [[-1, 2, -1, 2], 3],
     ])("max sum of %j => %i", (source, expected) => {
         const result = maxSubArray(source);
         assert.equal(result, expected);
@@ -48,5 +50,20 @@ describe.only("Maximum Subarray (time complexity limit: O(n))", () => {
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    return nums.reduce((sum, curr) => Math.max(sum, sum + curr, curr));
+    let globalMax = nums[0];
+    let max = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        // 1. 기존 max까지만 씀
+        // 2. 기존 max + 현재값 씀 (이어서 못 쓰게 해야 함 <- how?)
+        // --> 지금 case에서 2+2를 해버려서, 해결해줘야 함.
+        // --> 기존 것까지 다 더해서 쓰거나, 새로 시작해야 함.
+        // --> 흠.. 옛날의 max가 더 클 때가 있음... 이러면 케이스 제한이 어려운데? 아 이해함.
+        // --> globalMax랑 max를 분리하면 됨.
+        // 3. 현재값만 씀
+        max = Math.max(nums[i], max + nums[i]);
+        globalMax = Math.max(globalMax, max);
+    }
+
+    return globalMax;
 };
