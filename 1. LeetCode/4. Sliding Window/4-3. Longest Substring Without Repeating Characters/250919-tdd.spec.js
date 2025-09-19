11f4f974388e7e76a8f8140d9d8fb27ad528286f
Seongbin Kim <seongbin9786@gmail.com>
2025-09-19 14:05:27 +0900

    try(Longest Substring Without Repeating Characters): Map 기반 접근 - 최초 구현

--- 1. LeetCode/4. Sliding Window/4-3. Longest Substring Without Repeating Characters/250919-tdd.spec.js
+++ 1. LeetCode/4. Sliding Window/4-3. Longest Substring Without Repeating Characters/250919-tdd.spec.js
@@ -2,9 +2,12 @@ import { describe, it, assert } from "vitest";
 
 describe.only("Longest Substring Without Repeating Characters", () => {
     it.each([
+        // 기본 연속 Case
         ["ab", 2],
         ["aa", 1],
         ["abc", 3],
+        // 나중에 재등장하는 Case
+        ["ababa", 2],
     ])("%s => %i", (input, expected) => {
         const result = lengthOfLongestSubstring(input);
         assert.equal(result, expected);
@@ -16,14 +19,18 @@ describe.only("Longest Substring Without Repeating Characters", () => {
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
+    const prevIdxMap = new Map();
+
     let globalMax = 0;
-    let currMax = 1;
-    for (let i = 1; i < s.length; i++) {
-        if (s[i - 1] === s[i]) {
-            currMax = 1;
-        } else {
+    let currMax = 0;
+    for (let i = 0; i < s.length; i++) {
+        const prevIdx = prevIdxMap.get(s[i]);
+        if (prevIdx === undefined) {
             currMax++;
+        } else {
+            currMax = 1;
         }
+        prevIdxMap.set(s[i], i);
         globalMax = Math.max(globalMax, currMax);
     }
     return globalMax;
