d74f4095d6e91a6dc05be27cbb52ceb63ed7465a
Seongbin Kim <seongbin9786@gmail.com>
2025-10-05 17:53:03 +0900

    try(방금그곡): 풀이

--- 3. Programmers Again/방금그곡/250930-tdd.spec.js
+++ 3. Programmers Again/방금그곡/250930-tdd.spec.js
@@ -26,7 +26,10 @@ import { describe, it, assert } from "vitest";
 */
 describe("방금그곡", () => {
     it.each([
+        // 기본 TC
         ["a", ["00:00,00:01,answer,a"], "answer"],
+        ["a", ["00:00,00:01,answer,b"], "(None)"],
+        // 프로그래머스 TC
         [
             "ABCDEFG",
             ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"],
@@ -49,5 +52,67 @@ describe("방금그곡", () => {
 });
 
 function solution(m, musicinfos) {
-    return "answer";
+    // 1. 주어진 음악들에 대해 # 처리를 한다.
+    m = escapeSharp(m);
+
+    // 2. 주어진 음악들에 대해 재생 시간만큼 늘이거나 줄인다
+    // 3. map을 한다. [재생 시간, 원본 배열의 위치]를 추가한다.
+    musicinfos = musicinfos.map((info, index) => {
+        const [rawStartAt, rawEndAt, title, rawMusic] = info.split(",");
+        const startAt = parseTime(rawStartAt);
+        const endAt = parseTime(rawEndAt);
+        const duration = endAt - startAt;
+        const escapedMusic = escapeSharp(rawMusic);
+        const music =
+            escapedMusic.repeat(Math.floor(duration / escapedMusic.length)) +
+            escapedMusic.slice(0, duration % escapedMusic.length);
+
+        console.log(
+            `rawMusic: ${rawMusic} ==> music: ${music} (repeat: ${Math.floor(
+                escapedMusic.length / duration
+            )}) (duration: ${duration})`
+        );
+
+        return {
+            index,
+            duration,
+            title,
+            music,
+        };
+    });
+
+    // 4. include로 필터링하고, [재생 시간 내림차순, 원본 배열의 위치 오름차순]으로 정렬하고 첫 원소 반환
+    const sortedMusics = musicinfos.sort((a, b) => {
+        if (a.duration === b.duration) {
+            return a.index - b.index;
+        }
+
+        return b.duration - a.duration; // DESC
+    });
+
+    console.log(`m: ${m}, sortedMusics:${JSON.stringify(sortedMusics)}`);
+
+    const found = sortedMusics.find(({ music }) => {
+        console.log(`music: ${music}, m: ${m}`);
+        return music.includes(m);
+    });
+
+    return found?.title ?? "(None)";
+}
+
+function escapeSharp(original) {
+    const diff = "a".charCodeAt(0) - "A".charCodeAt(0);
+    const arr = [...original];
+    for (let i = 0; i < original.length; i++) {
+        if (arr[i] === "#") {
+            arr[i - 1] = String.fromCharCode(arr[i - 1].charCodeAt(0) + diff);
+            arr[i] = "";
+        }
+    }
+    return arr.join("");
+}
+
+function parseTime(hhmm) {
+    const [hours, minutes] = hhmm.split(":").map(Number);
+    return hours * 60 + minutes;
 }
