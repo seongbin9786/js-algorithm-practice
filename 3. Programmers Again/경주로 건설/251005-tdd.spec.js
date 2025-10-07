c2ddf2c3e56b00156d29c223439c20c7617a7efa
Seongbin Kim <seongbin9786@gmail.com>
2025-10-07 13:22:38 +0900

    try(경주로 건설): 다익스트라 풀이 (88/100)

--- 3. Programmers Again/경주로 건설/251005-tdd.spec.js
+++ 3. Programmers Again/경주로 건설/251005-tdd.spec.js
@@ -39,9 +39,12 @@ import { describe, it, assert } from "vitest";
 3. 결국 모든 경로를 조회는 하되, 불필요한 조회를 줄이는 가지치기가 필요함 (!!)
 4. 가지치기를 위해서는 캐싱이 필요하며, greedy, DP 기반의 다익스트라가 존재함
     - 다익스트라는 greedy, dp 기반의 알고리즘으로 PQ를 활용하면 시간복잡도가 O(E log V)이다.
-    - 256 * 6 = 1,536
-    --> 
-
+    - 이번에는 인접 행렬(N^2), 4방향이므로 O(4N^2 log V)이다.
+5. 단순 다익스트라와는 다르게, 방향 별로 구분해야 함
+    - [y,x]로 식별되는 각 좌표를 정점(vertex)으로 보고 4방향에 대한 edge가 있다고 볼 수 있음
+    - 길이는 어떤 edge(방향)에서 들어오든 상관이 없음.
+    --> 이렇게 풀면 88점이고, 내 TC에서도 한 개 틀림
+    --> 이렇게 하면 문제가 뭐냐면, '현재까지 비효율적인 경로'가 '이후' 효율적일 수 있다는 점임
 */
 describe("경주로 건설 (Lv.3)", () => {
     it.each([
@@ -54,16 +57,16 @@ describe("경주로 건설 (Lv.3)", () => {
         //     ],
         //     900,
         // ],
-        // [
-        //     [
-        //         [0, 0, 1, 0, 0],
-        //         [0, 0, 0, 0, 0],
-        //         [0, 1, 0, 1, 0],
-        //         [1, 0, 0, 0, 0],
-        //         [1, 0, 0, 0, 0],
-        //     ],
-        //     0,
-        // ],
+        [
+            [
+                [0, 0, 1, 0, 0], // down +100
+                [0, 0, 0, 0, 0], // right,right,down +600+100+600
+                [0, 1, 0, 1, 0], // down +100
+                [1, 0, 0, 0, 0], // down + 100
+                [1, 0, 0, 0, 0], // right,right + 600,100
+            ],
+            2300,
+        ],
         // [
         //     [
         //         [0, 0, 0, 0, 0, 0, 0, 0],
@@ -77,7 +80,7 @@ describe("경주로 건설 (Lv.3)", () => {
         //     ],
         //     1900,
         // ],
-        // 프로그래머스 TC
+        // // 프로그래머스 TC
         // [
         //     [
         //         [0, 0, 0],
@@ -101,10 +104,10 @@ describe("경주로 건설 (Lv.3)", () => {
         // ],
         // [
         //     [
-        //         [0, 0, 1, 0],
-        //         [0, 0, 0, 0],
-        //         [0, 1, 0, 1],
-        //         [1, 0, 0, 0],
+        //         [0, 0, 1, 0], // down +100
+        //         [0, 0, 0, 0], // right,right,down+600+100+600
+        //         [0, 1, 0, 1], // down + 100
+        //         [1, 0, 0, 0], // right + 600
         //     ],
         //     2100,
         // ],
@@ -127,64 +130,114 @@ describe("경주로 건설 (Lv.3)", () => {
 });
 
 function solution(board) {
-    let minCost = Infinity;
-
-    const dy = [0, 0, -1, 1];
-    const dx = [1, -1, 0, 0]; // 남,북,좌,우
+    const dx = [0, 0, -1, 1];
+    const dy = [1, -1, 0, 0]; // 남,북,좌,우
     const N = board.length;
-    const visited = Array.from({ length: N }, () => Array(N).fill(false));
-    let currCost = 0;
-    const prevDirections = [];
+    const dist = Array.from({ length: N }, () => Array(N).fill(Infinity));
+    const heap = new MinHeapShort((a, b) => a[2] - b[2]); // distance
 
-    console.log(`N:${N}`);
+    // dijkstra
+    dist[0][0] = 0;
+    if (board[0][1] === 0) {
+        dist[0][1] = 100;
+        heap.push([0, 1, 100, 3]);
+    }
 
-    function dfs(y, x) {
-        if (visited[y][x]) {
-            return;
-        }
+    if (board[1][0] === 0) {
+        dist[1][0] = 100;
+        heap.push([1, 0, 100, 0]);
+    }
+    while (!heap.isEmpty()) {
+        const [y, x, distance, prevDir] = heap.pop();
+        if (distance > dist[y][x]) continue;
 
         if (y === N - 1 && x === N - 1) {
-            minCost = Math.min(minCost, currCost);
-            console.log(`minCost update:`, minCost);
-            return;
+            return distance;
         }
 
-        console.log(`visiting:${y},${x}`);
-
-        visited[y][x] = true;
+        console.log(
+            `[pop] (${y},${x}), distance: ${distance}, prevDir: ${prevDir}`
+        );
 
         for (let dir = 0; dir < 4; dir++) {
             const nx = x + dx[dir];
             const ny = y + dy[dir];
 
             if (
+                // 되돌아가는 것 방지
+                !(
+                    (dir === 1 && prevDir === 0) ||
+                    (dir === 0 && prevDir === 1) ||
+                    (dir === 2 && prevDir === 3) ||
+                    (dir === 3 && prevDir === 2)
+                ) &&
                 nx >= 0 &&
                 nx < N &&
                 ny >= 0 &&
                 ny < N &&
-                !visited[ny][nx] &&
                 board[ny][nx] === 0
             ) {
-                const prevDir = prevDirections[prevDirections.length - 1];
-                const nextCost =
-                    prevDirections.length === 0
-                        ? 100
-                        : prevDir === dir
-                        ? 100
-                        : 600;
-
-                currCost += nextCost;
-                prevDirections.push(dir);
-                dfs(ny, nx);
-                prevDirections.pop();
-                currCost -= nextCost;
+                // 직전의 방향을 알아야 함.
+                const ndist = distance + (dir === prevDir ? 100 : 600);
+                console.log(
+                    `[push] (${y},${x} => ${ny},${nx}), prevDir:${prevDir} -> dir:${dir}, ndist: ${ndist}`
+                );
+                console.log(`heap: ${JSON.stringify(heap.arr)}`);
+                // 다른 방향으로 들어왔을 때 같은 값일 수 있음
+                if (ndist > dist[ny][nx]) continue;
+                dist[ny][nx] = ndist;
+                heap.push([ny, nx, ndist, dir]);
             }
         }
-
-        visited[y][x] = false;
     }
+}
 
-    dfs(0, 0);
+// 그대로 가져옴
+export function MinHeapShort(_comp) {
+    const f = Math.floor;
+    const arr = [undefined];
+    const comp = _comp ?? ((a, b) => a - b);
 
-    return minCost;
+    const push = (item) => {
+        arr.push(item);
+        let c = arr.length - 1;
+        while (c > 1) {
+            const p = f(c / 2);
+            if (comp(arr[c], arr[p]) >= 0) break;
+            [arr[c], arr[p]] = [arr[p], arr[c]];
+            c = p;
+        }
+    };
+
+    const pop = () => {
+        if (arr.length === 1) return;
+        const minVal = arr[1];
+
+        // NOTE: 주의<!> length=1일 때는 바로 반환해야 함.
+        const last = arr.pop();
+        if (arr.length === 1) return minVal;
+        arr[1] = last;
+        let i = 1;
+
+        while (true) {
+            const l = i * 2,
+                r = i * 2 + 1;
+            let min = i;
+            if (l < arr.length && comp(arr[l], arr[min]) < 0) min = l;
+            if (r < arr.length && comp(arr[r], arr[min]) < 0) min = r;
+            if (min === i) break;
+            [arr[min], arr[i]] = [arr[i], arr[min]];
+            i = min;
+        }
+        return minVal;
+    };
+
+    const isEmpty = () => arr.length === 1;
+
+    return {
+        isEmpty,
+        push,
+        pop,
+        arr,
+    };
 }
