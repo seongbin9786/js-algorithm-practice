86df6ec164a3bb82e4d1ff29da466eee032c5d23
Seongbin Kim <seongbin9786@gmail.com>
2025-11-07 17:45:31 +0900

    try(두 원 사이의 정수 쌍): 정답 이해, 효율성 통과

--- 3. Programmers Again/두 원 사이의 정수 쌍/251107-tdd.spec.js
+++ 3. Programmers Again/두 원 사이의 정수 쌍/251107-tdd.spec.js
@@ -11,7 +11,8 @@
 
 [발상2]
 - r2 <= 100만이라, 2중 포문을 사용할 수 없음
-- 
+    - 1축 기준 + 1사분면만 계산해서 4배
+- 이 계산이 되게 어려운데, 그냥 다음에 다시 풀 때 어떻게 풀지 또 고민하면서 배우면 좋을 듯
 
 */
 import { describe, it, assert } from "vitest";
@@ -25,21 +26,20 @@ describe("두 원 사이의 정수 쌍 (Lv.2)", () => {
 
 function solution(r1, r2) {
     let count = 0;
-    for (let y = -r2; y <= r2; y++) {
-        for (let x = -r2; x <= r2; x++) {
-            if (isInLargerCircle(y, x) && !isInSmallerCircle(y, x)) {
-                count++;
-            }
-        }
+    for (let y = 1; y <= r2; y++) {
+        const maxX = getAbsXForLargeCircle(y);
+        const minX = r1 >= y ? getAbsXForSmallCircle(y) : 0; // 굳이?
+        console.log(`y: ${y} => maxX:${maxX}, minX: ${minX}`);
+
+        count += (maxX - minX + 1) * 4;
     }
     return count;
 
-    function isInLargerCircle(y, x) {
-        return Math.pow(y, 2) + Math.pow(x, 2) <= Math.pow(r2, 2);
+    function getAbsXForLargeCircle(y) {
+        return Math.floor(Math.sqrt(r2 * r2 - y * y));
     }
 
-    // 둘레에 걸친 것 제외
-    function isInSmallerCircle(y, x) {
-        return Math.pow(y, 2) + Math.pow(x, 2) < Math.pow(r1, 2);
+    function getAbsXForSmallCircle(y) {
+        return Math.ceil(Math.sqrt(r1 * r1 - y * y));
     }
 }
